4c466d32e95e59b097d795742f49ae5d97b478ca
Min Ku Kang <tominku@gmail.com>
2023-07-12 15:50:52 +0900

    ..

--- src/P.2.6.2.cpp
+++ src/P.2.6.2.cpp
@@ -3,13 +3,11 @@
 #include <sciplot/sciplot.hpp>
 #include "util.cpp"
 using namespace arma; 
-using namespace std; 
-using namespace sciplot;
-typedef std::vector<double> stdvec;
 
 int main() {
 
     int N = 5;
+    double total_width = 5.0;
     int last_index = N - 1; 
     double e1 = 11.7;
     double e2 = 3.9;
@@ -20,6 +18,12 @@ int main() {
     A(last_index, last_index) = 1;
 
     // matrix construction
+    pairlist width_eps_list;    
+    width_eps_list.push_back(std::pair<double, double>(2.5, 0.01));
+    width_eps_list.push_back(std::pair<double, double>(2.5, 0.02));
+    
+    std::pair<mat, vec> A_b = construct_A_b_poisson(total_width, N, width_eps_list);   
+
     A(1, span(0, 2)) = {e1, -2*e1, e1};
     A(2, span(1, 3)) = {e1, -e2 - e1, e2};
     A(3, span(2, 4)) = {e2, -2*e2, e2};
---
+++ src/util.cpp
@@ -0,0 +1,91 @@
+#include <armadillo> 
+#include <iostream> 
+#include <sciplot/sciplot.hpp>
+using namespace arma; 
+using namespace std; 
+using namespace sciplot;
+typedef std::vector<double> stdvec;
+typedef std::vector < std::pair<double, double> > pairlist;
+
+// A function that matches node index i to permitivity (eps).
+// i starts from 1.
+double i_to_eps(double i, double total_width, int N, pairlist& width_eps_list)
+{   
+    double delta_x = total_width * 1.0 / double(N-1);
+    double physical_position = delta_x * (i - 1);
+    
+    pairlist::iterator it;   
+    double accumulated_width = 0.0;
+    double found_eps = -1.0;
+    for (it = width_eps_list.begin(); it != width_eps_list.end(); ++it) 
+    {
+        double width = (*it).first;  
+        double eps = (*it).second;
+        //std::cout << "*it " << " = " << (*it).first << " : " << (*it).second << std::endl;
+        accumulated_width += width;
+        if (physical_position <= accumulated_width)
+        {
+            found_eps = eps;
+            break;
+        }
+                      
+    }        
+    return found_eps;
+}
+
+std::pair<mat, vec> construct_A_b_poisson(double total_width, int N, pairlist& width_eps_list)
+{    
+    mat A(N, N, arma::fill::zeros);
+    int last_index = N - 1; 
+    
+    // matrix construction
+    A(0, 0) = 1;
+    A(last_index, last_index) = 1;
+
+    // matrix construction
+    for (int k=1; k<last_index; k++)
+    {
+        double i = double(k+1);
+        vec row_elements(3);
+        row_elements(0) = i_to_eps(i - 0.5, total_width, N, width_eps_list);
+        row_elements(1) = - ( i_to_eps(i - 0.5, total_width, N, width_eps_list) + i_to_eps(i + 0.5, total_width, N, width_eps_list) );
+        row_elements(2) = i_to_eps(i + 0.5, total_width, N, width_eps_list);
+        A(k, span(k-1, k+1)) = row_elements;
+    }
+
+    vec b(N, arma::fill::zeros);    
+    // b(last_index) = 1;   
+    
+    return std::pair<mat, vec>(A, b);    
+}
+
+void plot(int N, vec& y)
+{
+    stdvec solution_vec = conv_to<stdvec>::from(y);
+
+    Plot2D plot;
+    plot.xlabel("x");
+    plot.ylabel("y");
+
+    // Set the x and y ranges
+    //plot.xrange(0.0, 5);
+    //plot.yrange(-3, 3);
+
+    // Set the legend to be on the bottom along the horizontal
+    plot.legend()
+        .atOutsideBottom()
+        .displayHorizontal()
+        .displayExpandWidthBy(2);
+
+    Vec x = linspace(0.0, 5, N);
+    plot.drawCurve(x, solution_vec);
+    plot.drawPoints(x, solution_vec).pointType(6);
+    // Create figure to hold plot
+    Figure fig = {{plot}};
+    // Create canvas to hold figure
+    Canvas canvas = {{fig}};
+    canvas.size(800, 400);
+
+    // Show the plot in a pop-up window
+    canvas.show();  
+}
