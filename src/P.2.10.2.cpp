60537b17c7733282cb509cb1fde3f34b44934185
Min Ku Kang <tominku@gmail.com>
2023-07-17 16:06:00 +0900

    ...

---
+++ src/P.2.10.2.cpp
@@ -0,0 +1,90 @@
+#include <armadillo> 
+#include <iostream> 
+#include <sciplot/sciplot.hpp>
+#include "util.cpp"
+using namespace arma; 
+
+const double n_int = 1e10;    
+const double T = 300; // (K)
+const double V_T = k_B * T / q;
+
+double f(double phi, double dop)
+{            
+    double f_value = n_int * ( exp(phi / V_T) - exp(-phi / V_T) ) - dop;
+    return f_value;
+}
+
+double df_over_dx(double phi)
+{
+    double derivative_value = n_int * ( (1.0 / V_T)*exp(phi / V_T) - (-1.0 / V_T)*exp(-phi / V_T) );
+    return derivative_value;
+}
+
+void plot(stdvec &doping_list, stdvec &phis);
+
+int main() {
+
+    double x_0 = 1;    
+    double dop = 1e18;
+    int num_iters = 50;
+    stdvec xs;
+    stdvec residuals;
+
+    int num_experiments = 9;
+    vec phis(num_experiments);
+    vec doping_list(num_experiments);
+    doping_list[0] = 1e10;
+    for (int i=1; i<doping_list.n_elem; ++i)
+    {
+        doping_list[i] = doping_list[0] * pow(10, i); 
+    }
+
+    for (int k=0; k<doping_list.n_elem; ++k)
+    {
+        double x_i = x_0;
+        double dop = doping_list[k];
+        for (int i=0; i<num_iters; i++)
+        {
+            double residual = f(x_i, dop);
+            xs.push_back(x_i);        
+            residuals.push_back(residual);
+            double delta_x = (1 / df_over_dx(x_i) ) * ( - residual );
+            x_i += delta_x;                    
+        }
+        phis[k] = x_i;
+        printf("doping: 1e%d, x_sol: %f \n", 10+k, x_i);
+    }
+
+    stdvec std_doping_list = conv_to<stdvec>::from(doping_list);
+    stdvec std_phis = conv_to<stdvec>::from(phis);
+    plot(std_doping_list, std_phis);
+}
+
+
+void plot(stdvec &doping_list, stdvec &phis)
+{
+    Plot2D plot;
+
+    plot.xlabel("Doping Density (/cm^3)");
+    plot.ylabel("Electrostatic Potential(V)");
+
+    // Set the legend to be on the bottom along the horizontal
+    plot.legend()
+        .atOutsideBottom()
+        .displayHorizontal()
+        .displayExpandWidthBy(2);
+    plot.grid().show();
+
+    plot.xtics().logscale(10);
+
+    plot.drawPoints(doping_list, phis).pointType(6).pointSize(2);
+    plot.drawCurve(doping_list, phis);
+    // Create figure to hold plot
+    Figure fig = {{plot}};
+    // Create canvas to hold figure
+    Canvas canvas = {{fig}};
+    canvas.size(800, 400);
+
+    // Show the plot in a pop-up window
+    canvas.show(); 
+}
---
+++ src/P2.9.2.cpp
@@ -0,0 +1,57 @@
+#include <armadillo> 
+#include <iostream> 
+#include <sciplot/sciplot.hpp>
+#include "util.cpp"
+using namespace arma; 
+
+int main() {
+
+    int N = 61;
+    double total_width = 6.0;    
+
+    // matrix construction
+    pairlist width_eps_list;    
+    width_eps_list.push_back(std::pair<double, double>(0.5, 3.9));
+    width_eps_list.push_back(std::pair<double, double>(5, 11.7));
+    width_eps_list.push_back(std::pair<double, double>(0.5, 3.9));
+
+    // width_eps_list.push_back(std::pair<double, double>(1.5, 3.9));
+    // width_eps_list.push_back(std::pair<double, double>(3, 11.7));
+    // width_eps_list.push_back(std::pair<double, double>(1.5, 3.9));
+
+    std::pair<double, double> end_potentials(0.33374, 0.0);
+
+    std::pair<mat, vec> A_b = construct_A_b_poisson(total_width, N, width_eps_list, end_potentials);   
+
+    mat A = A_b.first;
+    vec b = A_b.second;
+    
+    double deltaX = 0.1 * 1e-9; // in meter    
+    double N_acceptor = 1e18 * 1e6; //     
+
+    for (int k=1; k<60; ++k)
+    {
+        int i = k + 1;
+        double c = deltaX * deltaX * q * N_acceptor / eps_0;
+        if (i < 6)
+            b(i) = 0.0; 
+        else if (i == 6)
+            b(i) = 0.5 * c; 
+        else if (i > 6 and i < 56)
+            b(i) = c;
+        else if (i == 56)
+            b(i) = 0.5 * c;
+        else if (i > 56)
+            b(i) = 0.0;
+    }
+
+    // matrix scaling
+    // double eps_0 = 8.854 * 1e-12; // in meter
+    // A.rows(1, N-2) = A.rows(1, N-2) / eps_0;
+    // b(span(1, N-2)) = b(span(1, N-2)) / eps_0;
+    
+    //A.print("A:");
+
+    vec sol_vec = arma::solve(A, b);  
+    plot(total_width, N, sol_vec);
+}
