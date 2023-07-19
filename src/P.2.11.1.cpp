7e27a19cc10bb85f01324b775768145f12563724
Min Ku Kang <tominku@gmail.com>
2023-07-19 18:44:33 +0900

    ...

--- src/P.2.11.1.cpp
+++ src/P.2.11.1.cpp
@@ -8,84 +8,77 @@ const int N = 61;
 double deltaX = 0.1e-9; // in meter  
 double dop = 1e18 * 1e6; // in meter    
 int n_int = 1e10 * 1e6;;
-double T = 300;
-int boundary_i_first = 6;
-int boundary_i_second = 56;
+double T = 300;    
+double total_width = 6.0;    
+double t_ox = 0.5;
+double t_si = 5;
+int interface1_i = 6;
+int interface2_i = 56;
+int si_begin_i = interface1_i + 1;
+int si_end_i = interface2_i - 1;
 
-// f(phi) or residual(phi)
-vec f(vec phi)
+// residual(phi): the size of r(phi) is (N - 2), but the size of phi is N.
+vec r(vec phi)
 {
-    int start_i = 1;
-    int last_i = N;    
-    vec phi_i_minus_1 = phi(span(0, last_i - 1 - 1));
-    vec phi_i_plus_1 = phi(span(1, last_i - 1));
-    vec r_i = (eps_si/deltaX) * (phi_i_plus_1 - 2*phi + phi_i_minus_1);
-    int idx1 = boundary_i_first + 1 - 1; 
-    int idx2 = boundary_i_first - 1 - 1; 
-    r_i(span(idx1, idx2)) += ( deltaX*q*dop - deltaX*q*n_int*exp(q*phi(span(idx1, idx2))/(k_B*T)) );
+    int start_i = 2;
+    int last_i = N-1;    
+    vec phi_i_minus_1 = phi(span(start_i - 1 - 1, last_i - 1 - 1));
+    vec phi_i = phi(span(start_i - 1, last_i - 1));
+    vec phi_i_plus_1 = phi(span(start_i - 1 + 1, last_i - 1 + 1));
+    vec r_i = (eps_si/deltaX) * (phi_i_plus_1 - 2.0*phi_i + phi_i_minus_1);     
+    r_i(span(si_begin_i-1, si_end_i-1)) += ( deltaX*q*dop - deltaX*q*n_int*exp(q*phi(span(si_begin_i-1, si_end_i-1))/(k_B*T)) );
     return r_i;
 }
 
+// the jacobian matrix size is (N - 2) by (N - 2)
 mat jacobian(vec phi)
 {
-                
+    mat jac(N-2, N-2, fill::zeros);    
+    int offset = 2;
+    for (int i=2; i<=(interface1_i - 1); ++i)
+    {
+        jac(i - offset, i + 1 - offset) = eps_ox / deltaX;
+        jac(i - offset, i - offset) =  -2.0 * eps_ox / deltaX;
+        jac(i - offset, i - 1 - offset) = eps_ox / deltaX; 
+    }
+
+    for (int i=si_begin_i; i<=si_end_i; ++i)
+    {
+        jac(i - 1, i + 1 - 1) = eps_si / deltaX;
+        jac(i - 1, i - 1) =  -2.0 * eps_si / deltaX - deltaX*q*n_int*exp(q*phi(i-1)/(k_B*T)) ;
+        jac(i - 1, i - 1 - 1) = eps_si / deltaX; 
+    }
+
+    for (int i=(interface2_i + 1); i<=(N-1); ++i)
+    {
+        jac(i - 1, i + 1 - 1) = eps_ox / deltaX;
+        jac(i - 1, i - 1) =  -2.0 * eps_ox / deltaX;
+        jac(i - 1, i - 1 - 1) = eps_ox / deltaX; 
+    }
+
+    return jac;
 }
 
 int main() {
 
-    int N = 61;
-    double total_width = 6.0;    
-    double t_ox = 0.5;
-    double t_si = 5;
-
-    // matrix construction
-    pairlist width_eps_list;        
-    width_eps_list.push_back(std::pair<double, double>(t_ox, 3.9));
-    width_eps_list.push_back(std::pair<double, double>(t_si, 11.7));
-    width_eps_list.push_back(std::pair<double, double>(t_ox, 3.9));
-
-    std::pair<double, double> end_potentials(0.33374, 0.33374);
-
-    std::pair<mat, vec> A_b = construct_A_b_poisson(total_width, N, width_eps_list, end_potentials);   
-
-    mat A = A_b.first;
-    vec b = A_b.second;
-    
-    double deltaX = 0.1 * 1e-9; // in meter    
-    double N_acceptor = 1e18 * 1e6; // in meter     
-
-    for (int i=1; i<=N; ++i)
-    {        
-        double c = deltaX * deltaX * q * N_acceptor / eps_0;
-        if (i > 1 && i < 6)
-            b(i-1) = 0.0; 
-        else if (i == 6) // ox-si boundary
-            b(i-1) = 0.5 * c; 
-        else if (i > 6 and i < 56)
-            b(i-1) = c;
-        else if (i == 56) // si-ox boundary
-            b(i-1) = 0.5 * c;
-        else if (i > 56 && i < N)
-            b(i-1) = 0.0;
+    vec phi_0(N, arma::fill::ones);
+    int num_iters = 10;
+    //mat xs(num_iters, 3, arma::fill::zeros); // each row i represents the solution at iter i.
+    //mat residuals(num_iters, 3, arma::fill::zeros); // each row i represents the residual at iter i.    
+    vec phi_i = phi_0;
+    for (int i=0; i<num_iters; i++)
+    {
+        vec residual = r(phi_i);
+        mat jac = jacobian(phi_i);
+        printf("test");
+        // xs.row(i) = x_i.t();         
+        // residuals.row(i) = residual.t();
+        // mat jac = jacobian(x_i);
+        // vec delta_x = arma::solve(jac, -residual);
+        // x_i += delta_x;        
+        
+        // printf("[iter %d]   detal_x: %f   residual: %f\n", i, max(abs(delta_x)), max(abs(residual)));        
     }
 
-    vec sol_vec = arma::solve(A, b);      
-
-    // Potential
-    plot_args args;
-    args.total_width = total_width;
-    args.N = N;    
-    args.y_label = "Potential (V)";
-    plot(sol_vec, args);
-
-    // Electron Density
-    double T = 300;
-    double n_int = 1e10;
-    double segment_width = total_width / (N - 1); // in nanometer
-    int ox_si_boundary_k = int(t_ox / segment_width);
-    int si_ox_boundary_k = int((t_ox + t_si) / segment_width);
-    vec ed(N, arma::fill::zeros);
-    ed(span(ox_si_boundary_k, si_ox_boundary_k)) = n_int * exp(q * sol_vec(span(ox_si_boundary_k, si_ox_boundary_k)) / (k_B * T));
-    args.y_label = "Electron Density (/cm^3)";
-    plot(ed, args);
+    //x_i.print("found solution (phi):");
 }
